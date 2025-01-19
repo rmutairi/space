e43e23ac37414caecb165876e7a87b4eeea025c6
rmutairi <168989497+rmutairi@users.noreply.github.com>
2025-01-19 17:11:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js';
-import PositionAlongPathState from '../positionAlongPathTools/PositionAlongPathState.js';
-import { handleScroll, updatePosition } from '../positionAlongPathTools/PositionAlongPathMethods.js';
+import PositionAlongPathState from './positionAlongPathTools/PositionAlongPathState.js';
+import { handleScroll, updatePosition } from './positionAlongPathTools/PositionAlongPathMethods.js';
 import { loadCurveFromJSON } from './curveTools/CurveMethods.js';
 
 
